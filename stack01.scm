0f37cfed8160624e7eb865ac199e50023bb8b7c8
mugsimon <mug24375826dev@gmail.com>
2018-06-14 15:49:21 +0900

    add repl, modify VM and compile-lookup

--- stack01.scm
+++ stack01.scm
@@ -38,7 +38,7 @@
         (cond 
           [(null? vars) (nxtrib (cdr e) (+ rib 1))]
           [(eq? (car vars) var) (return rib elt)]
-          [else (nxtelt (cdr vars) (+ elt 1))])))))
+[else (nxtelt (cdr vars) (+ elt 1))])))))
 
 (define functional
   (lambda (body e)
@@ -109,7 +109,8 @@
 		 [test (then else)
 		       (VM a (if a then else) e s)]
 		 [assign (n m x)
-			 (index-set! (find-link n e) m a) (VM a x e s)]
+			 (index-set! (find-link n e) m a)
+			 (VM a x e s)]
 		 [frame (ret x)
 			(VM a x e (push ret (push e s)))]
 		 [argument (x)
@@ -117,7 +118,7 @@
 		 [apply ()
 			(record (body link) a
 				(VM a body s (push link s)))]
-		 [return (n)
+		 [return (n) ;;n=[args]+[static link]
 			 (let ([s (- s n)])
 			   (VM a (index s 0) (index s 1) (- s 2)))])))
 
@@ -131,8 +132,31 @@
   (lambda (x)
     (VM '() (compile x '() '(halt)) 0 0)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define c.scm:debug
+  (lambda (a x e s)
+    (print "a:" a)
+    (print "x:" x)
+    (print "e:" e)
+    (print "s:" s)
+    (c.scm:stack-print s)
+    (print "----------------------")))
 
-(define stack-repl
+(define c.scm:stack-print
+  (lambda (s)
+    (display "stack:")
+    (if (= s 0)
+	(print)
+	(let loop ((i (- s 1)))
+	  (if (= i 0)
+	      (print (vector-ref stack i))
+	      (begin (display (vector-ref stack i))
+		     (display " ")
+		     (loop (- i 1))))))))
+
+(define *top-level-continuation* #f)
+
+(define repl
   (lambda ()
     (call/cc
      (lambda (quit)
@@ -144,3 +168,127 @@
 	       (quit 'bye)
 	       (begin (print (evaluate input))
 		      (loop)))))))))
+
+(define c.scm:read-eval-print
+  (lambda (cont)
+    (set! *top-level-continuation* cont)
+    (c.scm:prompt)
+    (print (evaluate (read)))
+    #t))
+
+(define c.scm:prompt
+  (lambda ()
+    (display "c.scm> ")
+    (flush)))
+
+(define c.scm:repl
+  (lambda ()
+    (let loop ()
+      (if (call/cc c.scm:read-eval-print)
+	  (loop)
+	  (print "bye")))))
+
+(define c.scm:error
+  (lambda (msg)
+    (print msg)
+    (*top-level-continuation* #t)))
+
+(define compile-lookup
+  (lambda (var e return)
+    (if (null? e)
+	(c.scm:error "c.scm:error - Unbound variable")
+	(recur nxtrib ([e e] [rib 0])
+	       (if (null? e)
+		   (c.scm:error "c.scm:error - Unbound variable")
+		   (recur nxtelt ([vars (car e)] [elt 0])
+			  (cond 
+			   [(null? vars)	
+			    (nxtrib (cdr e) (+ rib 1))]
+			   [(eq? (car vars) var)
+			    (return rib elt)]
+			   [else
+			    (nxtelt (cdr vars) (+ elt 1))])))))))
+
+(define VM 
+  (lambda (a x e s)
+    (c.scm:debug a x e s) ;;debug code
+    (record-case x
+		 [halt ()
+		       a]
+		 [refer (n m x)
+			(VM (index (find-link n e) m) x e s)]
+		 [constant (obj x)
+			   (VM obj x e s)]
+		 [close (body x)
+			(VM (functional body e) x e s)]
+		 [test (then else)
+		       (VM a (if a then else) e s)]
+		 [assign (n m x)
+			 (index-set! (find-link n e) m a)
+			 (VM a x e s)]
+		 [frame (ret x)
+			(VM a x e (push ret (push e s)))]
+		 [argument (x)
+			   (VM a x e (push a s))]
+		 [apply ()
+			(record (body link) a
+				(VM a body s (push link s)))]
+		 [return (n) ;;n=[args]+[static link]
+			 (let ([s (- s n)])
+			   (VM a (index s 0) (index s 1) (- s 2)))]
+		 [else
+		  (print "Unknown VM instruction")
+		  a])))
+
+#|
+(define compile-define-lookup
+  (lambda (var e return)
+    (if (null? e)
+	(c.scm:error "c.scm:error - Unbound variable")
+	(recur nxtrib ([e e] [rib 0])
+	       (if (null? e)
+		   (c.scm:error "c.scm:error - Unbound variable")
+		   (recur nxtelt ([vars (car e)] [elt 0])
+			  (cond 
+			   [(null? vars)	
+			    (nxtrib (cdr e) (+ rib 1))]
+			   [(eq? (car vars) var)
+			    (return rib elt)]
+			   [else
+			    (nxtelt (cdr vars) (+ elt 1))])))))))
+
+(define compile 
+  (lambda (x e next)
+    (cond 
+     [(symbol? x) 
+      (compile-lookup x e 
+		      (lambda (n m)
+			(list 'refer n m next)))]
+     [(pair? x)
+      (record-case x 
+		   [quote (obj)
+			  (list 'constant obj next)]
+		   [lambda (vars body)
+		     (list 'close
+			   (compile body 
+				    (extend e vars)
+				    (list 'return (+ (length vars) 1)))
+			   next)]
+		   [if (test then else)
+		       (let ([thenc (compile then e next)]
+			     [elsec (compile else e next)])
+			 (compile test e (list 'test thenc elsec)))]
+		   [set! (var x)
+			 (compile-lookup var e
+					 (lambda (n m) (compile x e (list 'assign n m next))))]
+		   [else
+		    (recur loop ([args (cdr x)]
+				 [c (compile (car x) e '(apply))])
+			   (if (null? args)
+			       (list 'frame next c)
+			       (loop (cdr args)
+				     (compile (car args)
+					      e 
+					      (list 'argument c)))))])]
+     [else (list 'constant x next)])))
+|#
